7be4b3c1281d4df6349edc04d54801398fab858c
takumi <armktk0329m@gmail.com>
2019-09-08 16:22:56 +0900

    herokuエラー修正

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,6 +4,7 @@
 // that code so it'll be compiled.
 // import "bootstrap-honoka/dist/css/bootstrap.min.css"
 import "bootstrap-honoka/dist/js/bootstrap.min.js"
+import 'stylesheets/application'
 
 require("@rails/ujs").start()
 require("turbolinks").start()
