1a13bb420eb7438bce8580e3d517d99f1ca9e139
takumi <armktk0329m@gmail.com>
2019-10-02 16:41:55 +0900

    Fix:エラー修正（モデル）

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,28 +1,3 @@
-# class ProductsController < ApplicationController
-#   before_action :set_product, only[:show, :edit, :update, :destroy]
-
-
-
-#   def show
-#   end
-
-#   def edit
-#   end
-
-#   def update
-#     if @product.update(product_params)
-#       redirect_to products_path
-#     else
-#       render 'edit'
-#     end
-#   end
-
-#   def destroy
-#     @product.destroy
-#     redirect_to products_path
-#   end
-
-
 class ProductsController < ApplicationController
   before_action :set_product, only:[:show, :edit, :update, :destroy]
 
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,5 @@
 class Product < ApplicationRecord
     has_rich_text :content
-    include ImageUploader[:image]
     belongs_to :user
     validates :song_title, presence: true
     validates :title, presence: true
