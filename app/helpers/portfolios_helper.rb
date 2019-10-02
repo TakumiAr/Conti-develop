3f233b4aeed94d9885f50e0844ffb627aadb1e0f
takumi <armktk0329m@gmail.com>
2019-10-02 17:30:47 +0900

    Fix: http✗ https◯

--- app/helpers/portfolios_helper.rb
+++ app/helpers/portfolios_helper.rb
@@ -8,7 +8,7 @@ module PortfoliosHelper
             youtube_id = $5
         end
         
-        %Q{<iframe title="YouTube video player" width="640" height="390" src="http://www.youtube.com/embed/#{ youtube_id }" frameborder="0" allowfullscreen></iframe>}
+        %Q{<iframe title="YouTube video player" width="640" height="390" src="https://www.youtube.com/embed/#{ youtube_id }" frameborder="0" allowfullscreen></iframe>}
     end
 
     def youtube_embed_free(youtube_url)
